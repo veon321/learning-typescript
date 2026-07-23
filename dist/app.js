de261557d9f98962f63cb74abbf4955bef7a7b82
unknown <veon@vp.pl>
2026-07-23 22:17:04 +0200

    learning-typescript

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,1 @@
 "use strict";
-const buttonElement = document.getElementById("buy");
-const calculatePrice = (originalPrice, hasDiscount) => {
-    return hasDiscount ? originalPrice * 0.8 : originalPrice;
-};
-buttonElement.addEventListener("click", () => {
-    const originalPrice = 50;
-    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
-    console.log(calculatePrice(originalPrice, hasDiscount));
-});
