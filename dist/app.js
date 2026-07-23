0d3f0c7c598e631afed06d91ab62f1029adc59d8
unknown <veon@vp.pl>
2026-07-23 22:22:03 +0200

    learning-typescript

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,14 @@
 "use strict";
+/*        <li>
+          <label for="task-1">Wyrzucić śmieci</label>
+          <input type="checkbox" id="task-1" name="Wyrzucić śmieci" />
+        </li>
+      */
+// Wyrzucić śmieci
+// Pójść na siłownie
+// Nakarmić koty
+const tasks = [
+    "Wyrzucić śmieci",
+    "Pójść na siłownie",
+    "Nakarmić koty",
+];
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+/*        <li>
+          <label for="task-1">Wyrzucić śmieci</label>
+          <input type="checkbox" id="task-1" name="Wyrzucić śmieci" />
+        </li>
+      */
+
+// Wyrzucić śmieci
+// Pójść na siłownie
+// Nakarmić koty
+
+const tasks: string[] = [
+  "Wyrzucić śmieci",
+  "Pójść na siłownie",
+  "Nakarmić koty",
+];
