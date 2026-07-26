8bb956200b44443e814502cfdc4fff74d0a444b4
unknown <veon@vp.pl>
2026-07-26 15:31:59 +0200

    learning-typescript

--- dist/app.js
+++ dist/app.js
@@ -20,6 +20,9 @@ const render = () => {
         checkboxElement.name = task.name;
         checkboxElement.id = id;
         checkboxElement.checked = task.done;
+        checkboxElement.addEventListener("change", () => {
+            task.done = !task.done;
+        });
         taskElement.appendChild(labelElement);
         taskElement.appendChild(checkboxElement);
         tasksContainerElement.appendChild(taskElement);
