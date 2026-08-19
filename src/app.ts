47f79bee61ebe52b1661840c0e23e40739f14817
unknown <veon@vp.pl>
2026-08-19 18:12:15 +0200

    learning-typescript

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ interface Task {
 const tasks: Task[] = [
   { title: "Wyrzucić śmieci", done: false, category: "hobby" },
   { title: "Pójść na siłownie", done: true, category: "gym" },
-  { title: "Nakarmić koty", done: false, category: "work" },
+  { title: "Nakarmić koty", done: true, category: "work" },
 ];
 
 const categories: Category[] = ["general", "work", "gym", "hobby"];
